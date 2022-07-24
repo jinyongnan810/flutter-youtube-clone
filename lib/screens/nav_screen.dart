662fb66579db501445056d095d8cce7ba028357c
Kin Yuunan <yuunan.kin@gmail.com>
2022-07-24 09:00:06 +0900

    stack & offstage

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -11,6 +11,23 @@ class NavScreen extends StatefulWidget {
 
 class _NavScreenState extends State<NavScreen> {
   int _currentIndex = 0;
+  final List<Widget> _screens = [
+    const Scaffold(
+      body: Center(child: Text('Home')),
+    ),
+    const Scaffold(
+      body: Center(child: Text('Search')),
+    ),
+    const Scaffold(
+      body: Center(child: Text('Add')),
+    ),
+    const Scaffold(
+      body: Center(child: Text('Subscriptions')),
+    ),
+    const Scaffold(
+      body: Center(child: Text('Library')),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,31 +41,42 @@ class _NavScreenState extends State<NavScreen> {
               });
             },
             items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_outlined),
-            activeIcon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_outlined),
-            activeIcon: Icon(Icons.add),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.subscriptions_outlined),
-            activeIcon: Icon(Icons.subscriptions),
-            label: 'Subscriptions',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books_outlined),
-            activeIcon: Icon(Icons.library_books),
-            label: 'Library',
-          ),
-        ]));
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                activeIcon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search_outlined),
+                activeIcon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_outlined),
+                activeIcon: Icon(Icons.add),
+                label: 'Add',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.subscriptions_outlined),
+                activeIcon: Icon(Icons.subscriptions),
+                label: 'Subscriptions',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.library_books_outlined),
+                activeIcon: Icon(Icons.library_books),
+                label: 'Library',
+              ),
+            ]),
+        body: Stack(
+            children: _screens
+                .asMap()
+                .map((index, screen) => MapEntry(
+                    index,
+                    Offstage(
+                      offstage: index != _currentIndex,
+                      child: screen,
+                    )))
+                .values
+                .toList()));
   }
 }
