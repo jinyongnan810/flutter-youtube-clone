2dc14e7ce4cc3608b672f198d24ecc5d46a3017a
Kin Yuunan <yuunan.kin@gmail.com>
2022-07-26 08:47:17 +0900

    SliverList & VideoCard

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube_clone/data.dart';
 import 'package:flutter_youtube_clone/widgets/custom_sliver_app_bar.dart';
+import 'package:flutter_youtube_clone/widgets/video_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: CustomScrollView(
-      slivers: [CustomSliverAppBar()],
+      slivers: [
+        const CustomSliverAppBar(),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((ctx, index) {
+          return VideoCard(video: videos[index]);
+        }, childCount: videos.length)),
+      ],
     ));
   }
 }
---
+++ lib/widgets/video_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_youtube_clone/data.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class VideoCard extends StatelessWidget {
+  final Video video;
+  const VideoCard({Key? key, required this.video}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(children: [
+          Image.network(
+            video.thumbnailUrl,
+            fit: BoxFit.cover,
+            height: 220,
+            width: double.infinity,
+          ),
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              color: Colors.black54,
+              child: Text(
+                video.duration,
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ]),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                  onTap: () => print('Move to profile'),
+                  child: CircleAvatar(
+                    foregroundImage: NetworkImage(video.author.profileImageUrl),
+                  )),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                        child: Text(
+                      video.title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(fontSize: 16),
+                    )),
+                    Flexible(
+                      child: Text(
+                        '${video.author.username} · ${video.viewCount} views · ${timeago.format(video.timestamp)}',
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption!
+                            .copyWith(fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                child: const Icon(Icons.more_vert),
+                onTap: () => print('move to more'),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
